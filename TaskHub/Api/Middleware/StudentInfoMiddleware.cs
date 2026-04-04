c2d982454385c7f4eb4182191d853e8ba50cc711
Марина <mulkooomik@gmail.com>
2026-04-05 01:32:12 +0400

    add response time and student info middleware

---
+++ TaskHub/Api/Middleware/StudentInfoMiddleware.cs
@@ -0,0 +1,24 @@
+﻿namespace Api.Middleware
+{
+    public class StudentInfoMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public StudentInfoMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            context.Response.OnStarting(() =>
+            {
+                context.Response.Headers["X-Student-Name"] = "Tumashova Marina";
+                context.Response.Headers["X-Student-Group"] = "RI-240912";
+                return Task.CompletedTask;
+            });
+
+            await _next(context);
+        }
+    }
+}
